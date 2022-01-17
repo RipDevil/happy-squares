ff21ded981a5fd6d067b71d569a35565da3c7838
RipDevil <tiberi64@yandex.ru>
2022-01-18 01:26:40 +0700

    prettier

--- potato/go-squares.js
+++ potato/go-squares.js
@@ -1,27 +1,29 @@
-import logger from "./services/verboseLogger/index.js";
-import opts from "./services/opts/index.js";
+import logger from './services/verboseLogger/index.js';
+import opts from './services/opts/index.js';
 
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
 export default function main() {
-        logger.write("The app has been started");
-        logger.write("From " + process.cwd());
+        logger.write('The app has been started');
+        logger.write('From ' + process.cwd());
         logger.write(JSON.stringify(opts, null, 3));
 
         const readStaticHtmlStream = fs.createReadStream(
-                path.join("./potato/static/index.html")
+                path.join('./potato/static/index.html')
         );
-        const writeResultHtmlStream = fs.createWriteStream(path.join("happy-square.html"));
+        const writeResultHtmlStream = fs.createWriteStream(
+                path.join('happy-square.html')
+        );
 
         readStaticHtmlStream.pipe(writeResultHtmlStream);
 
-        writeResultHtmlStream.on("error", (err) => {
-                logger.write('Write '+err.message);
+        writeResultHtmlStream.on('error', (err) => {
+                logger.write('Write ' + err.message);
         });
 
-        readStaticHtmlStream.on("error", (err) => {
-                logger.write('Read '+err.message);
+        readStaticHtmlStream.on('error', (err) => {
+                logger.write('Read ' + err.message);
         });
 }
 
