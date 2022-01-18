3b0c140d5a6c45a85f6f1c705bba5f7083d4f6f5
RipDevil <tiberi64@yandex.ru>
2022-01-18 10:03:00 +0700

    prettying the promised pipeline

--- potato/utils/promised-pipeline.js
+++ potato/utils/promised-pipeline.js
@@ -1,6 +1,6 @@
 import { pipeline } from 'stream';
 
 export const promisedPipeline = (rs, ws) =>
-        new Promise((res, rej) => {
-                pipeline(rs, ws, res).on('error', rej);
+        new Promise((resolve, reject) => {
+                pipeline(rs, ws, resolve).on('error', reject);
         });
