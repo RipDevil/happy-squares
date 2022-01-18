98cce0962bb191fe5e52b38d83a0a2037b49d12a
RipDevil <tiberi64@yandex.ru>
2022-01-18 09:36:38 +0700

    prettier

--- potato/go-squares.js
+++ potato/go-squares.js
@@ -1,26 +1,26 @@
-import logger from "./services/verboseLogger/index.js";
-import opts from "./services/opts/index.js";
+import logger from './services/verboseLogger/index.js';
+import opts from './services/opts/index.js';
 
-import { mkdir } from "fs/promises";
-import { pipeline } from "stream";
-import { join as pathJoin } from "path";
-import { createReadStream, createWriteStream } from "fs";
+import { mkdir } from 'fs/promises';
+import { pipeline } from 'stream';
+import { join as pathJoin } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
 
 const root = process.cwd();
 
 export default async function main() {
-        logger.write("The app has been started");
-        logger.write("From " + root);
+        logger.write('The app has been started');
+        logger.write('From ' + root);
         logger.write(JSON.stringify(opts, null, 3));
 
-        const pathToDistHtml = pathJoin(root, "dist", "happy-squares.html");
-        const pathToSrcHtml = pathJoin(root, "potato/static", "index.html");
-        const pathToDistDir = pathJoin(root, "dist");
+        const pathToDistHtml = pathJoin(root, 'dist', 'happy-squares.html');
+        const pathToSrcHtml = pathJoin(root, 'potato/static', 'index.html');
+        const pathToDistDir = pathJoin(root, 'dist');
 
         try {
                 await mkdir(pathToDistDir);
         } catch (err) {
-                logger.write(`Error: ${err?.message || ""}`);
+                logger.write(`Error: ${err?.message || ''}`);
         }
 
         pipeline(
@@ -28,7 +28,7 @@ export default async function main() {
                 createWriteStream(pathToDistHtml),
                 () => {
                         logger.write(
-                                "Error: an error has occuered during pipeline"
+                                'Error: an error has occuered during pipeline'
                         );
                 }
         );
