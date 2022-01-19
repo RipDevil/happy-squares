525aaeb85ad98d1eccd97eb893d12224a796db69
RipDevil <tiberi64@yandex.ru>
2022-01-19 12:06:02 +0700

    added some additional info for commits in static

--- potato/utils/write-squares.js
+++ potato/utils/write-squares.js
@@ -1,7 +1,7 @@
 export function* writeSquares(commits) {
     for (const commit in commits) {
         yield `
-            <div data-date="${commit}" data-count="${commits[commit]}"></div>
+            <div data-date="${commit}" data-count="${commits[commit]}" title="${commit} - Commits: ${commits[commit]}"></div>
         `;
     }
 }
