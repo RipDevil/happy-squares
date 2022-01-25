3f8d7b754e19968e7b502c58f33bfb5fe91e984a
RipDevil <tiberi64@yandex.ru>
2022-01-25 19:50:48 +0700

    refactor execTerminal

--- potato/utils/exec-terminal.js
+++ potato/utils/exec-terminal.js
@@ -3,13 +3,11 @@ import { exec } from 'child_process';
 export const execTerminal = (command) => {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
-            if (error != null) {
-                return reject(new Error(error));
-            } else if (typeof stderr != 'string') {
-                return reject(new Error(stderr));
-            } else {
-                return resolve(stdout);
+            if (error || stderr === 'string') {
+                return reject(new Error(error || stderr));
             }
+
+            return resolve(stdout);
         });
     });
 };
